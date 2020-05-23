8393c9d573c70ca857d3363960385dba234c344b
charieScript <gozione3@gmail.com>
2020-05-23 11:02:53 +0100

    added populated item to data structure

--- app.js
+++ app.js
@@ -26,6 +26,26 @@ const ItemCtrl = (function () {
         getItems: function () {
             return data.items;
         },
+        addItem: function (name, calories) {
+            let ID;
+            // generate iDs 
+            if (data.items > 0) {
+                ID = data.items[data.items.length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+
+            // parse calories as number
+            calories = parseInt(calories);
+
+            // create new Item
+            let newItem = new Item(ID, name, calories);
+
+            // add new item to array
+            data.items.push(newItem)
+
+            return newItem;
+        },
         logData: function () {
             return data;
         }
@@ -38,7 +58,10 @@ const ItemCtrl = (function () {
 const UICtrl = (function () {
     // ui selectors (object)
     const UIselectors = {
-        itemList: '#item-list'
+        itemList: '#item-list',
+        addBtn: '.add-btn',
+        itemNameInput: '#item-name',
+        itemCaloriesInput: '#item-calories'
     }
 
     // public method
@@ -58,24 +81,57 @@ const UICtrl = (function () {
 
             // insert list item
             document.querySelector(UIselectors.itemList).innerHTML = html;
+        },
+        getItemInput: function () {
+            return {
+                name: document.querySelector(UIselectors.itemNameInput).value,
+                calories: document.querySelector(UIselectors.itemCaloriesInput).value
+            }
+        },
+
+        getSelectors: function() {
+            return UIselectors;
         }
+
     }
 })();
 
 
 // APP CONTROLLER //
 const App = (function (ItemCtrl, UICtrl) {
-    
+    // load event listerners
+    const loadEventListerners = function() {
+        const UIselectors = UICtrl.getSelectors();
+
+        // add item event 
+        document.querySelector(UIselectors.addBtn).addEventListener('click', itemAddSubmit)
+    }
+
+    // add item submit
+    const itemAddSubmit = function (e) {
+        // get form input from ui controller
+        const input = UICtrl.getItemInput();
+
+        // check for name and calorie input
+        if (input.name !== '' && input.calories !== '')  {
+            // add item 
+            const newItem = ItemCtrl.addItem(input.name, input.calories);
+        }
+
+        e.preventDefault();
+    }
+
     // initialiser for the app (public methods)
     return {
         init: function () {
-            console.log('initialise app....');
-
             // fetch items from data structure
             const item = ItemCtrl.getItems();
 
             //populate items list 
             UICtrl.populateItemList(item)
+
+            // loadeventlisterners
+            loadEventListerners()
         }
     }
 
